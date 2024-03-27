4352e51fc813e0bd0e58ab73e6ec3963a20836bc
Antoni Wójcik <wojcikantek@gmail.com>
2024-03-27 11:28:33 +0100

    bug fixes, readme update

--- Policy.py
+++ Policy.py
@@ -14,50 +14,55 @@ class Policy:
         self.policy_name: str = aws_iam_role_policy["PolicyName"]
         self.policy_document: dict = aws_iam_role_policy["PolicyDocument"]
 
-    def __validate_json(self, json_object) -> None:
+    def __validate_json(self, json_object: dict) -> None:
         def validate_policy_name(policy_name) -> None:
             if not isinstance(json_object["PolicyName"], str):
                 raise ValueError("PolicyName must be a string")
-            if 1 >= len(policy_name) >= 128:
+            if not (1 <= len(policy_name) <= 128):
                 raise ValueError("PolicyName must be between 1 and 128 characters")
-            if not re.match(r"[\w+=,.@-]+", policy_name):
+            if not re.fullmatch(r"[\w+=,.@-]+", policy_name):
                 raise ValueError("PolicyName must be alphanumeric")
 
         def validate_policy_document(policy_document) -> None:
-            if not isinstance(json_object["PolicyDocument"], dict):
+            if not isinstance(policy_document, dict):
                 raise ValueError("PolicyDocument must be a dictionary")
-            if json_object["PolicyDocument"].keys() != {"Version", "Statement"}:
+            if "Version" not in policy_document or "Statement" not in policy_document:
                 raise KeyError(
-                    "Invalid PolisyDocument inside JSON, check the JSON structure."
+                    """Invalid PolisyDocument inside JSON, check the JSON structure. Keys "Version" and "Statement" are required."""
                 )
-            if not isinstance(json_object["PolicyDocument"]["Statement"], list):
-                raise ValueError("Statement must be a list")
+            if not isinstance(policy_document["Statement"], list):
+                raise ValueError("Statement in PolicyDocument must be a list")
 
-            for statement in json_object["PolicyDocument"]["Statement"]:
+            for statement in policy_document["Statement"]:
                 if not isinstance(statement, dict):
-                    raise ValueError("Statement must be a dictionary")
+                    raise ValueError("Each statement must be a dictionary")
 
         if json_object.keys() != {"PolicyName", "PolicyDocument"}:
-            raise KeyError("Invalid input JSON key, check the JSON structure.")
+            raise KeyError(
+                "Invalid input JSON key, check the JSON structure. Keys 'PolicyName' and 'PolicyDocument' are required."
+            )
 
         validate_policy_name(json_object["PolicyName"])
         validate_policy_document(json_object["PolicyDocument"])
 
     def verify_resource_data(self) -> bool:
-
         for statement in self.policy_document["Statement"]:
             # Verify that the policy document contains the required resource key
             if "Resource" in statement:
                 # check if the resource field contains a single *
                 if statement["Resource"] == "*":
-                    return True
-        return False
+                    return False
+        return True
 
     def __str__(self) -> str:
         return json.dumps(self.__dict__, indent=2)
 
 
 if __name__ == "__main__":
-    test = open("ex_json.json").read()
-    policy = Policy(test)
-    print(policy.verify_resource_data())
+    data = open("ex_json.json").read()
+    try:
+        policy = Policy(data)
+        print(policy)
+    except Exception as e:
+        print(e)
+    pass
--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # AWS-IAM-RolePolicy-validator
 Little Python script to verify AWS::IAM::Role Policy JSON
+
+## Requirements
+- Python installed on your system (tested with Python 3.12.2)
+
+## How to run 
+1. Check if Python is installed by running `python -V` command.
+2. If so, create virtual environment or use global environment.
+3. Run `validator.py` script with file name or path to JSON you want check with command:
+    ```bash
+    python validator.py <json_file/path>
+    ```
+
+## AWS::IAM::Role Policy JSON specification
+
+### Syntax
+```JSON
+{
+  "PolicyDocument" : Json,
+  "PolicyName" : String
+}
+```
+
+### Properties
+
+#### PolicyDocument
+```
+Required: Yes
+Type: JSON
+```
+
+#### PolicyName
+```
+Required: Yes
+Type: String
+Pattern: [\w+=,.@-]+
+Minimum: 1
+Maximum: 128
+```
+
+#### Example of correct AWS::IAM::Role Policy JSON
+```JSON
+{
+    "PolicyName": "root",
+    "PolicyDocument": {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "IamListAccess",
+                "Effect": "Allow",
+                "Action": [
+                    "iam:ListRoles",
+                    "iam:ListUsers"
+                ],
+                "Resource": "*"
+            }
+        ]
+    }
+}
+```
+[Link for further documentation](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-iam-role-policy.html)
+
+### Self-Assessment
+[x] Method veryfying the input JSON data <br>
+[x]Readme includes "how to run" instructions <br>
+[x] Input data format is defined as AWS::IAM::Role Policy <br>
+[x] Unit tests<br>
+[x] Covering edge cases<br>
+
+### Author
+[Antoni Wójcik](https://github.com/AntuanW)
